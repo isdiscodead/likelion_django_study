5fbbb2ef96dc38a79a0fb01506e0e205741715b9
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-01 02:59:18 +0900

    djust django course 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/helloworld.html')
     # alt+enter로 import 가능
