abdd8ad463325a4c600544ea984e1c6ce5184d37
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-02 00:20:55 +0900

    Read practice

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render # alt+enter로 import 가능
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -6,12 +8,15 @@ from accountapp.models import HelloWorld
 def hello_world(request):
 
     if request.method == "POST":
+        # create
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
-        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
