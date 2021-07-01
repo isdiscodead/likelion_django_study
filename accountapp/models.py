ebb06cc23822ede16659489726bb713d97254a82
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-01 20:58:16 +0900

    DB & Model link

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 클래스 하나가 item 하나가 된다 !
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)  # 공백 X 문자열 필드
