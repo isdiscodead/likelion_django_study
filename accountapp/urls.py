91ebb8d61d24b6f1d89e13a75302eb8d4cd43885
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-03 00:33:37 +0900

    login & logout

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -22,4 +23,6 @@ app_name = "accountapp"
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('login/', LoginView.as_view(template_name="accountapp/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- almighty/settings.py
+++ almighty/settings.py
@@ -13,6 +13,8 @@ from pathlib import Path
 import os, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ### 환경 변수 설정 ###
@@ -155,3 +157,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# login&logout redirect
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
