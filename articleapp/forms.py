0c5e80eeb2289b5c56aeef2c6cbcc02a17270235
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-05 03:01:48 +0900

    CRUD article

---
+++ articleapp/forms.py
@@ -0,0 +1,10 @@
+import django.forms
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'img', 'content']
--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    # 회원 탈퇴 시 작성자는 알 수 없음 등으로 표시
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    title = models.CharField(max_length=200, null=True)
+    img = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
