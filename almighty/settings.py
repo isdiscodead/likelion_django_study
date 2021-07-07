b890e0bf39f26c1b025daaee98e96bd0095ad9fa
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-07 21:57:57 +0900

    debug some codes and edit home

--- almighty/settings.py
+++ almighty/settings.py
@@ -170,5 +170,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # login&logout redirect
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:home')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
--- projectapp/models.py
+++ projectapp/models.py
@@ -6,3 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    # 객체 이름 str로 만들기
+    def __str__(self):
+        return f'{self.pk} : {self.title}'
