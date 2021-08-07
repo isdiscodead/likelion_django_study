78078558c3f9ca0ee48d37c5c3d24465e0870da3
isdiscodead <isdiscodead@korea.ac.kr>
2021-08-07 20:34:12 +0900

    add transaction

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.urls import reverse
@@ -12,24 +14,31 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    if LikeRecord.objects.filter(user=user, article=article).exists():
+        raise ValidationError('Like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+    article.like += 1
+    article.save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('articleapp:detail', kwargs={'pk':kwargs['pk']})
+        return reverse('articleapp:detail', kwargs={'pk': kwargs['pk']})
 
     def get(self, *args, **kwargs):
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
 
-        if LikeRecord.objects.filter(user=user, article=article).exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, "좋아요가 반영되었습니다.")
+        except ValidationError:
             messages.add_message(self.request, messages.ERROR, "좋아요는 한 번만 가능합니다.")
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, "좋아요가 반영되었습니다.")
 
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
